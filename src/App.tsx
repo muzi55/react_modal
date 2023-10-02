6211bedc8a513a500ae83eaa34ea78a4974be480
muzi55 <muzi5@kakao.com>
2023-10-02 23:12:57 +0900

    모달창 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./components/modal/Modal";
 
 function App() {
+  const [modalState, setModalState] = useState<boolean>(false);
+
+  const handleModalOpenBtn = () => {
+    setModalState(true);
+  };
   return (
     <>
       <h1>모달창</h1>
-      <Modal> </Modal>
+      <button onClick={handleModalOpenBtn}>모달창 열기</button>
+      {modalState && (
+        <Modal modalState={modalState} setModalState={setModalState}>
+          <h2>오늘의 반찬</h2>
+          <p>제육볶음, 김치, 콩자반</p>
+        </Modal>
+      )}
     </>
   );
 }
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import "./modal.css";
 interface Props {
   children: React.ReactNode;
+  modalState: boolean;
+  setModalState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Modal = ({ children }: Props): JSX.Element => {
+const Modal = ({ children, modalState, setModalState }: Props): JSX.Element => {
+  const handleModalCloseBtn = () => {
+    setModalState(false);
+  };
   return (
     <>
-      <dialog open className="modal-container">
+      <dialog open={modalState} className="modal-container">
+        <button onClick={handleModalCloseBtn} className="modal-close__button">
+          X
+        </button>
         {children}
       </dialog>
-      <div className="modal-bg"></div>
+      <div onClick={handleModalCloseBtn} className="modal-bg"></div>
     </>
   );
 };
